82e1ace61a53db90976b803a0183005e4e4a87e2
Antonio Cervelione <antonio_cervelione_reply@whirlpool.com>
2020-12-30 15:01:41 +0100

    Set "show differences" from cms

--- react/components/comparisonPageRow/ProductSummaryRow.tsx
+++ react/components/comparisonPageRow/ProductSummaryRow.tsx
@@ -7,17 +7,22 @@ import { Checkbox } from 'vtex.styleguide'
 import ComparisonProductContext from '../../ComparisonProductContext'
 import './row.css'
 
+interface ProductSummaryRowProps{
+  isShowDifferenceDefault:boolean;
+}
+ 
+
 const CSS_HANDLES = [
   'productSummaryRowContainer',
   'fieldNameCol',
   'showDifferencesContainer',
 ]
 
-const ProductSummaryRow = () => {
+const ProductSummaryRow = ({isShowDifferenceDefault}:ProductSummaryRowProps) => {
   const cssHandles = useCssHandles(CSS_HANDLES)
-
+  const [isShowDifferenceByDefault, changesChecked] = useState(isShowDifferenceDefault)
   const [showDifferences, setShowDifferences] = useState(false)
-
+  console.log("state shown->"+showDifferences)
   const {
     useProductComparisonState,
     useProductComparisonDispatch,
@@ -40,11 +45,12 @@ const ProductSummaryRow = () => {
     const showDifferences =
       comparisonData.products &&
       comparisonData.products.length > 1 &&
-      comparisonData.showDifferences
+      (comparisonData.showDifferences || isShowDifferenceByDefault)
     setShowDifferences(showDifferences)
   }, [comparisonData])
-
+  debugger
   const onSelectorChanged = (e: { target: { checked: boolean } }) => {
+    changesChecked(!isShowDifferenceByDefault)
     dispatchComparison({
       args: {
         showDifferences: e.target.checked,
@@ -79,5 +85,17 @@ const ProductSummaryRow = () => {
     </div>
   )
 }
-
+ProductSummaryRow.schema = {
+  title: 'editor.product-summary-row.title',
+  description: 'editor.product-summary-row.description',
+  type: 'object',
+  properties: {
+    isShowDifferenceDefault:{
+      title:'Show difference',
+      description:'',
+      default:false,
+      type:'boolean'
+    }
+  }
+}
 export default ProductSummaryRow
